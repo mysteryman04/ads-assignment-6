2d5cbc83811798dbb33f02884d255ca5e1d712f2
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-29 23:15:30 +0600

    Declaring instance variable of class. The outer Map represents the source vertices, and the inner Map represents the destination vertices along with their corresponding edge weights. The constructor initializes the adjacencyMap as an empty HashMap

---
+++ src/WeightedGraph.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class WeightedGraph {
+    private Map<Vertex, Map<Vertex, Integer>> adjacencyMap;
+
+    public WeightedGraph() {
+        this.adjacencyMap = new HashMap<>();
+    }
+
+
+}

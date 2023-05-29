e5becc0b92a9f881d86444159d3f7a9cf9a8d089
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-30 00:02:34 +0600

    It takes the source vertex, destination vertex, and the weight of the edge as parameters.  the method checks if both the source and destination vertices are present in the adjacencyMap. If either of them is not present, it throws an IllegalArgumentException indicating that the vertices are not present in the graph.

--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -10,6 +10,13 @@ public class WeightedGraph {
     public void addVertex(Vertex vertex) {
         adjacencyMap.put(vertex, new HashMap<>());
     }
+    public void addEdge(Vertex source, Vertex destination, int weight) {
+        if (!adjacencyMap.containsKey(source) || !adjacencyMap.containsKey(destination))
+            throw new IllegalArgumentException("Vertices not present");
+
+        adjacencyMap.get(source).put(destination, weight);
+        adjacencyMap.get(destination).put(source, weight);
+    }
 
 
 }

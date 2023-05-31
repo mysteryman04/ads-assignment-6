3cf7cbf5508c8ced7df0c6874ab7da233bf3d43b
mysteryman04 <112248124+mysteryman04@users.noreply.github.com>
2023-05-31 23:42:00 +0600

    Add edges between the vertices with corresponding weights: graph.addEdge(vertexA, vertexB, 10);, graph.addEdge(vertexB, vertexC, 5);, graph.addEdge(vertexA, vertexC, 8); Create an instance of BreadthFirstSearch with the graph and the source vertex: BreadthFirstSearch bfs = new BreadthFirstSearch(graph, vertexA); Find the path from the source vertex to the destination vertex: List<Vertex> bfsPath = bfs.pathTo(vertexC); If a path exists, print the path: System.out.println("BFS Path: " + bfsPath);, otherwise print "No path found."

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WeightedGraph graph = new WeightedGraph();
+
+        Vertex vertexA = new Vertex(1);
+        Vertex vertexB = new Vertex(2);
+        Vertex vertexC = new Vertex(3);
+
+        graph.addVertex(vertexA);
+        graph.addVertex(vertexB);
+        graph.addVertex(vertexC);
+
+        graph.addEdge(vertexA, vertexB, 10);
+        graph.addEdge(vertexB, vertexC, 5);
+        graph.addEdge(vertexA, vertexC, 8);
+
+
+        BreadthFirstSearch bfs = new BreadthFirstSearch(graph, vertexA);
+        List<Vertex> bfsPath = bfs.pathTo(vertexC);
+
+        if (bfsPath != null) {
+            System.out.println("BFS Path: " + bfsPath);
+        } else {
+            System.out.println("No path found.");
+        }
+
+
+
+
     }
 }
--- src/WeightedGraph.java
+++ src/WeightedGraph.java
@@ -11,10 +11,19 @@ public class WeightedGraph {
         adjacencyMap.put(vertex, new HashMap<>());
     }
     public void addEdge(Vertex source, Vertex destination, int weight) {
+        if (!adjacencyMap.containsKey(source) || !adjacencyMap.containsKey(destination))
+            throw new IllegalArgumentException("Vertices not present in the graph.");
+
+        if (adjacencyMap.get(source) == null)
+            adjacencyMap.put(source, new HashMap<>());
+
+        if (adjacencyMap.get(destination) == null)
+            adjacencyMap.put(destination, new HashMap<>());
 
         adjacencyMap.get(source).put(destination, weight);
         adjacencyMap.get(destination).put(source, weight);
     }
+
     public int getEdgeWeight(Vertex source, Vertex destination) {
         if (!adjacencyMap.containsKey(source) || !adjacencyMap.containsKey(destination))
             throw new IllegalArgumentException("Vertices not present");
